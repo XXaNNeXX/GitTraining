999104b2e5bd09fd3ecee1d18569088bc52e0b4a
neuefische <info@neuefische.de>
2023-09-08 11:34:49 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(addition(3,2));
+    }
+
+    public static int addition(int a, int b) {
+
+        return a + b;
+    }
+}
