32e785728b170ea0eba2d3847f41b02a4d7450d8
neuefische <info@neuefische.de>
2023-09-08 11:40:25 +0200

    Add multiplication

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(addition(3,2));
+        System.out.println(multiplication(5, 8));
     }
 
     public static int addition(int a, int b) {
-
         return a + b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
     }
 }
